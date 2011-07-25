466ecdf4d0e728a1bc53f7d56c391900aa7bf2bd
Asheesh Laroia <asheesh@asheesh.org>
2011-07-25 11:56:23 +0200

    Add a test for basic signup

--- debexpo/tests/functional/test_register.py
+++ debexpo/tests/functional/test_register.py
@@ -1,7 +1,24 @@
-from debexpo.tests import *
+from debexpo.tests import TestController, url
+from debexpo.model import meta
+from debexpo.model.users import User
 
 class TestRegisterController(TestController):
 
-    def test_index(self):
-        response = self.app.get(url(controller='register', action='index'))
-        # Test response...
+    def test_maintainer_signup(self):
+        count = meta.session.query(User).filter(User.email=='mr_me@example.com').count()
+        self.assertEquals(count, 0)
+
+        self.app.post(url(controller='register', action='maintainer'),
+                                 {'name': 'Mr. Me',
+                                  'password': 'password',
+                                  'password_confirm': 'password',
+                                  'commit': 'yes',
+                                  'email': 'mr_me@example.com'})
+
+        count = meta.session.query(User).filter(User.email=='mr_me@example.com').count()
+        self.assertEquals(count, 1)
+
+        user = meta.session.query(User).filter(User.email=='mr_me@example.com').one()
+        # delete it
+        meta.session.delete(user)
+
