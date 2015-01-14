ecfea60cab448b70bd3f13dce1de03b5a4e66f89
Philip James <philip@eventbrite.com>
2015-01-13 20:19:46 -0800

    Adjust test.ini, provision.sh so tests pass...
    ...in vagrant.
    
    Tests pass in travis because a different ini file was used. This will
    be fixed in a upcoming commit.
    
    This commit causes all outbound mail to be discarded in vagrant. This
    will hopefully be fixed in an upcoming commit

--- provision.sh
+++ provision.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+# For some reasons the tests want this.
+mkdir -p ~/.gnupg
 cd /home/vagrant/debexpo/
 sudo apt-get update
-sudo apt-get install python-lxml libapt-pkg-dev python-pip python-dev python-virtualenv --yes
+sudo apt-get install postfix python-lxml libapt-pkg-dev python-pip python-dev python-virtualenv --yes
 sudo apt-get build-dep python-lxml --yes
+echo '* discard:' | sudo sh -c 'cat > /etc/postfix/discard-transport'
+if ! grep -q transport_maps /etc/postfix/main.cf; then
+    echo 'transport_maps = hash:/etc/postfix/discard-transport' | sudo sh -c 'cat >> /etc/postfix/main.cf'
+fi
+sudo postmap /etc/postfix/discard-transport
+sudo service postfix restart
 virtualenv venv
 . venv/bin/activate
 pip install https://launchpad.net/python-apt/main/0.7.8/+download/python-apt-0.8.5.tar.gz
 pip install --editable .
-python setup.py develop
 paster setup-app development.ini
 python setup.py compile_catalog
