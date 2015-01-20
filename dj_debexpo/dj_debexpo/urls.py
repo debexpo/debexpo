a082ace8b882475376c67f98ec401d74b7113e02
Philip James <philip@eventbrite.com>
2015-01-19 16:02:30 -0800

    Redo django app to use debian packages.

--- dj_debexpo/dj_debexpo/urls.py
+++ dj_debexpo/dj_debexpo/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'expo.views.home', name='home'),
+    # url(r'^$', 'dj_debexpo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+)
--- dj_debexpo/dj_debexpo/wsgi.py
+++ dj_debexpo/dj_debexpo/wsgi.py
@@ -1,14 +1,14 @@
 """
-WSGI config for expo project.
+WSGI config for dj_debexpo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_debexpo.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
