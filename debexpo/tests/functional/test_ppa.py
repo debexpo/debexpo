66d2c9a5ba52f43808840c6bfe3f87b9fe6f673c
Baptiste BEAUPLAT <lyknode@cilg.org>
2018-10-11 20:31:42 +0200

    Remove ppa controller test (ppa controller removed by e2cfc0cf91cb67608e00cbae365e18101c863f03)

--- debexpo/tests/functional/test_ppa.py
+++
@@ -1,7 +0,0 @@
-from debexpo.tests import *
-
-class TestPpaController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='ppa', action='index'))
-        # Test response...
