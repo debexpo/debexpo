073d24f7a23eb4e0575341743bff9f8cf5d5e56a
Baptiste BEAUPLAT <lyknode@cilg.org>
2019-02-13 14:52:41 +0100

    Add skeleton for sql migration scripts

---
+++ contrib/database/versions/7cceb63f746e_base.py
@@ -0,0 +1,25 @@
+"""Base
+
+Revision ID: 7cceb63f746e
+Revises: 
+Create Date: 2019-02-13 14:07:03.774616
+
+This file exists to allow downgrading from the first real version
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7cceb63f746e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
