bf682d754824eebcb8fe4aca92ab4e11276aff5d
Baptiste BEAUPLAT <lyknode@cilg.org>
2019-09-17 15:22:32 +0200

    Register base:index view

--- debexpo/settings/common.py
+++ debexpo/settings/common.py
@@ -22,6 +22,7 @@ BASE_DIR = path.dirname(
 INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debexpo.base'
 ]
 
 MIDDLEWARE = [
--- debexpo/urls.py
+++ debexpo/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.urls import path
+from django.urls import path
+from debexpo.base.views import index
 
 urlpatterns = [
+    path('', index, name='index')
 ]
