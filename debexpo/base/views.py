ac48f2f20770c608b9d7412392becf82fda1dfbb
Baptiste BEAUPLAT <lyknode@cilg.org>
2019-09-17 15:21:46 +0200

    Add base:index view

--- debexpo/base/views.py
+++ debexpo/base/views.py
@@ -27,5 +27,10 @@
 #   OTHER DEALINGS IN THE SOFTWARE.
 
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {
+        'settings': settings
+    })
