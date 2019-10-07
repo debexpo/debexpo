6c1f82ab630cfdee66f6bbd7dab609bd3dfb2d25
Baptiste BEAUPLAT <lyknode@cilg.org>
2019-10-07 22:00:17 +0200

    Check for user existance on registration

--- debexpo/accounts/forms.py
+++ debexpo/accounts/forms.py
@@ -27,6 +27,7 @@
 #   OTHER DEALINGS IN THE SOFTWARE.
 
 from django import forms
+from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 
 
@@ -39,14 +40,30 @@ class RegistrationForm(forms.Form):
                                      choices=[('maintainer', _('Maintainer')),
                                               ('sponsor', _('Sponsor'))])
 
-    def clean(self):
-        email = self.cleaned_data.get('email')
-        account_type = self.cleaned_data.get('account_type')
-
+    def _validate_sponsor_account(self, account_type, email):
         if (account_type and email and account_type == 'sponsor' and
                 not email.endswith('@debian.org')):
             self.add_error('account_type', _("A sponsor account must be "
                                              "registered with your @debian.org "
                                              "address"))
+
+    def _validate_uniqueness(self, name, email):
+        if name and User.objects.filter(first_name=name):
+            self.add_error('name', _('A user with this name is already '
+                                     'registered on the system. If it is you, '
+                                     'use that account!  Otherwise use a '
+                                     'different name to register.'))
+
+        if email and User.objects.filter(email=email):
+            self.add_error('email', _('A user with this email address is '
+                                      'already registered on the system'))
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        name = self.cleaned_data.get('name')
+        account_type = self.cleaned_data.get('account_type')
+
+        self._validate_sponsor_account(account_type, email)
+        self._validate_uniqueness(name, email)
 
         return self.cleaned_data
