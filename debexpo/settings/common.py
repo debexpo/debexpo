a468996cbe2b65f1606b50932b4a282d1537b933
Baptiste BEAUPLAT <lyknode@cilg.org>
2020-01-12 19:08:58 +0100

    Add settings for celery

--- debexpo/settings/common.py
+++ debexpo/settings/common.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
     'debexpo.base',
     'debexpo.accounts',
     'debexpo.keyring',
@@ -142,3 +143,15 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # GPG settings
 GPG_PATH = '/usr/bin/gpg'
+
+# Celery settings
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_BEAT_SCHEDULER = 'django'
+
+# Tasks beats
+TASK_IMPORTER_BEAT = 60 * 15  # Every 15 minutes
