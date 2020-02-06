71c09f5f079f0fccb8a5ca98aae72774b9b93178
Baptiste BEAUPLAT <lyknode@cilg.org>
2020-02-06 13:02:14 +0000

    Convert CleanupAccounts to django

--- debexpo/accounts/tasks.py
+++ debexpo/accounts/tasks.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-#   py.template - template for new .py files
+#   tasks.py - tasks for accounts
 #
 #   This file is part of debexpo
 #   https://salsa.debian.org/mentors.debian.net-team/debexpo
 #
-#   Copyright © 2019 Jonny Lamb <jonny@debian.org>
+#   Copyright © 2020 Baptiste BEAUPLAT <lyknode@cilg.org>
 #
 #   Permission is hereby granted, free of charge, to any person
 #   obtaining a copy of this software and associated documentation
@@ -28,55 +26,36 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 #   OTHER DEALINGS IN THE SOFTWARE.
 
-__author__ = 'Baptiste BEAUPLAT'
-__copyright__ = 'Copyright © 2019 Baptiste BEAUPLAT'
-__license__ = 'MIT'
+from datetime import timedelta, datetime, timezone
+from celery.task import PeriodicTask
+from logging import getLogger
 
-from datetime import timedelta, datetime
-from debexpo.cronjobs import BaseCronjob
-from debexpo.model import meta
-from debexpo.model.users import User
+from django.conf import settings
 
-# This import is used by the orm to map the DELETE ON CASCADE
-# between Users and PasswordReset
-from debexpo.model.password_reset import PasswordReset # NOQA
+from debexpo.accounts.models import User
+
+log = getLogger(__name__)
 
 
-class CleanupAccounts(BaseCronjob):
-    def setup(self):
-        self.db = meta.session
-        if 'token_expiration_days' in self.config:
-            self.expire_in = int(self.config['token_expiration_days'])
-        else:
-            self.expire_in = 7
+class CleanupAccounts(PeriodicTask):
+    run_every = timedelta(minutes=settings.TASK_CLEANUPACCOUNTS_BEAT)
 
-    def teardown(self):
-        pass
+    def __init__(self):
+        self.expire_in = int(settings.REGISTRATION_EXPIRATION_DAYS)
 
-    def invoke(self):
+    def run(self):
         accounts = self._list_account_expired()
-        count = self._remove_expired_accounts(accounts)
+        count = accounts.count()
+
         if count > 0:
-            self.log.info('{} accounts not activated removed.'.format(count))
+            accounts.delete()
+            log.info('{} accounts not activated removed.'.format(count))
 
     def _list_account_expired(self):
-        # SQLAchemly has to work with != None, so no pep8 here
-        return self.db.query(User) \
-            .filter(User.verification != None) \
-            .filter(User.lastlogin < self._get_deadline()) # NOQA
+        return User.objects.filter(
+            password__startswith='!',
+            date_joined__lt=self._get_deadline(),
+            is_active=True)
 
     def _get_deadline(self):
-        return datetime.now() - timedelta(days=self.expire_in)
-
-    def _remove_expired_accounts(self, accounts):
-        count = 0
-        for account in accounts.all():
-            self.db.delete(account)
-            count += 1
-
-        self.db.commit()
-        return count
-
-
-cronjob = CleanupAccounts
-schedule = timedelta(minutes=60)
+        return datetime.now(timezone.utc) - timedelta(days=self.expire_in)
