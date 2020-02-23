8493dc661a1307292db5f57c04a9a6f6889ba1d3
Baptiste BEAUPLAT <lyknode@cilg.org>
2020-02-23 23:31:57 +0100

    Add ClientDebianArchive and ClientFTPMasterAPI to communicate with Debian services

---
+++ debexpo/tools/clients/ftp_master.py
@@ -0,0 +1,52 @@
+#   ftp_master.py - Client for ftp-master API
+#
+#   This file is part of debexpo
+#   https://salsa.debian.org/mentors.debian.net-team/debexpo
+#
+#   Copyright © 2020 Baptiste BEAUPLAT <lyknode@cilg.org>
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation
+#   files (the "Software"), to deal in the Software without
+#   restriction, including without limitation the rights to use,
+#   copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following
+#   conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#   OTHER DEALINGS IN THE SOFTWARE.
+
+from debexpo.tools.files import CheckSumedFile
+from debexpo.tools.clients import ClientJsonAPI
+import debexpo.repository.models as repository
+
+
+class ClientFTPMasterAPI(ClientJsonAPI):
+    api = 'https://api.ftp-master.debian.org'
+
+    def get_origin_files(self, name, version):
+        pool = repository.Repository.get_pool(name)
+        route = f'{self.api}/file_in_archive/{pool}/{name}' \
+                f'/{name}_{version}%25.orig%25.tar%25'
+        origin_files = []
+
+        content = self.fetch_json_resource(route)
+
+        for match in content:
+            if 'filename' in match and 'sha256sum' in match:
+                origin = CheckSumedFile(match['filename'])
+                origin.add_checksum('sha256', match['sha256sum'])
+
+                origin_files.append(origin)
+
+        return origin_files
