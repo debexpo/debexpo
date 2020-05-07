9c204d33c656c2f70436077d9f28f6cf57e58cc4
Baptiste BEAUPLAT <lyknode@cilg.org>
2020-05-07 11:02:09 +0200

    Convert rfs plugin to django

--- debexpo/plugins/rfs.py
+++ debexpo/plugins/rfs.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-#   rfstemplate.py - collect information for RFS template plugin
+#   rfs.py - collect information for RFS template plugin
 #
 #   Copyright © 2016 Hayashi Kentaro <kenhys@gmail.com>
+#   Copyright © 2020 Baptiste BEAUPLAT <lyknode@cilg.org>
 #
 #   Permission is hereby granted, free of charge, to any person
 #   obtaining a copy of this software and associated documentation
@@ -25,101 +24,43 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 #   OTHER DEALINGS IN THE SOFTWARE.
 
-"""
-Holds the RFS template plugin.
-"""
-
-__author__ = 'Hayashi Kentaro'
-__copyright__ = 'Copyright © 2016 Hayashi Kentaro'
-__license__ = 'MIT'
-
-import logging
-import os
-
-from debian import copyright
-from debexpo.lib import constants
-from debexpo.plugins import BasePlugin
-
-log = logging.getLogger(__name__)
+from debexpo.plugins.models import BasePlugin, PluginSeverity
 
 
-class RfsTemplatePlugin(BasePlugin):
+class PluginRFS(BasePlugin):
+    @property
+    def name(self):
+        return 'rfs'
 
-    def _extract_copyright_info(self):
-        info = {}
-        copyright_path = os.path.join(self.tempdir,
-                                      "extracted/debian/copyright")
-        try:
-            with open(copyright_path, 'r') as f:
-                try:
-                    context = copyright.Copyright(f)
-                except copyright.NotMachineReadableError:
-                    return info
-
-                header = context.header
-                if header.upstream_contact:
-                    info['author'] = header.upstream_contact[0]
-
-                lic = header.license
-                if lic:
-                    info['license'] = lic.synopsis
-
-                if not lic:
-                    default_license = context.find_files_paragraph('*')
-                    if default_license:
-                        info['license'] = default_license.license.synopsis
-        except IOError as e:
-            log.warning('{}'.format(e))
-
-        return info
-
-    def test_rfs_template(self):
+    def run(self, changes, source):
         """
         Tests whether there are enough information for RFS template.
 
         This plugin collects:
 
         - Upstream Author (from debian/copyright)
-        - URL (from debian/control)
         - License (from debian/copyright)
-        - Changelog (from debian/changelog)
-
         """
-        log.debug('Checking whether there are enough information for RFS '
-                  'template')
-        upstream_author = "[fill in name and email of upstream]"
-        upstream_license = "[fill in]"
-        package_changelog = "[your most recent changelog entry]"
+        outcome = "d/copyright is in DEP5 format"
+        severity = PluginSeverity.info
 
-        outcome = "RFS: license, author and changelog parsed"
-        severity = constants.PLUGIN_SEVERITY_INFO
+        author = getattr(source.copyright, 'author')
+        licenses = getattr(source.copyright, 'licenses')
 
-        package_changelog = self.changes.get('Changes', '').strip()
+        if not author:
+            outcome = "Upstream-Contact missing from d/copyright"
+            severity = PluginSeverity.warning
 
-        if len(package_changelog.split('\n')) > 2:
-            package_changelog = '\n'.join(package_changelog.split('\n')[2:])
-
-        copyright_info = self._extract_copyright_info()
-
-        if 'author' in copyright_info:
-            upstream_author = copyright_info['author']
+        if not licenses:
+            licenses = None
+            outcome = "d/copyright is not in DEP5 format"
+            severity = PluginSeverity.error
         else:
-            outcome = "RFS: author info missing"
-            severity = constants.PLUGIN_SEVERITY_WARNING
-
-        if 'license' in copyright_info:
-            upstream_license = copyright_info['license']
-        else:
-            outcome = "RFS: no dep5 license"
-            severity = constants.PLUGIN_SEVERITY_ERROR
+            licenses = ', '.join(licenses)
 
         data = {
-            'upstream-author': upstream_author,
-            'upstream-license': upstream_license,
-            'package-changelog': package_changelog,
+            'author': author,
+            'licenses': licenses,
         }
 
-        self.passed(outcome, data, severity)
-
-
-plugin = RfsTemplatePlugin
+        self.add_result('copyright', outcome, data, severity)
