7134c872aeb60af626859779e92b6e641f94a6da
Baptiste BEAUPLAT <lyknode@cilg.org>
2020-06-08 20:30:17 +0200

    Refactoring for GitStorage
    Split into two classes:
    GitStorage for high level actions on the git storage (install, diff or
    remove).
    GitBackendDulwich that implements dulwich storage specifics.

--- debexpo/tools/gitstorage.py
+++ debexpo/tools/gitstorage.py
@@ -22,79 +22,106 @@
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #   DEALINGS IN THE SOFTWARE.
 
-from dulwich.objects import Tree, Commit, parse_timezone
+from io import BytesIO
+from os import makedirs, walk
+from os.path import isdir, join, relpath
+from logging import getLogger
+from shutil import rmtree, copytree
+
 from dulwich.repo import Repo
 from dulwich.patch import write_tree_diff
-from time import time
-from io import BytesIO
-import os
-
-from django.conf import settings
+from dulwich.index import get_unstaged_changes
 
 fileToIgnore = []
+log = getLogger(__name__)
 
 
 class NoOlderContent(Exception):
     pass
 
 
+class GitStorage():
+    def __init__(self, git_storage_path):
+        self.git_storage_path = git_storage_path
+
+    def install(self, source):
+        if not self.git_storage_path:
+            return
+
+        repository = join(self.git_storage_path,
+                          source.control.source['Source'])
+        git = GitBackendDulwich(repository)
+
+        self._sync_source(source, repository)
+        changes = self._list_files(repository)
+
+        git.stage(changes, True)
+        ref = git.commit()
+
+        return ref
+
+    # def remove(self, source):
+    #     pass
+
+    # def diff(self, upload_from, upload_to):
+    #     pass
+
+    def _sync_source(self, source, repository):
+        dest = join(repository, 'sources')
+
+        if isdir(dest):
+            rmtree(dest)
+
+        try:
+            copytree(source.get_source_dir(), dest)
+        except IOError:
+            pass
+
+    def _list_files(self, repository):
+        dest = join(repository, 'sources')
+        files = set()
+
+        for (root, _, filenames) in walk(dest):
+            for filename in filenames:
+                fpath = join(root, filename)
+                files.add(relpath(fpath, repository))
+
+        return files
+
+
 class GitBackendDulwich():
-    def _ignoreFile(self, dirName, fileName):
-        """
-        used for the copTree stuff
-        ``dirName``
-            the working directory
-        ``fileName``
-            list of files inside the directory (dirName)
-        """
-        result = []
-        for i in fileName:
-            path = dirName + i
-            if path not in fileToIgnore:
-                result.append(path)
-        return result
-
-    def _commit(self, tree):
-        """
-        commit a tree used only by the init
-        ``tree``
-            tree to commit
-        """
-        commit = Commit()
-        commit.tree = tree.id
-        commit.encoding = "UTF-8"
-        commit.committer = commit.author = settings.DEFAULT_FROM_EMAIL
-        commit.commit_time = commit.author_time = int(time())
-        tz = parse_timezone('-0200')[0]
-        commit.commit_timezone = commit.author_timezone = tz
-        commit.message = " "
-        self.repo.object_store.add_object(tree)
-        self.repo.object_store.add_object(commit)
-        self.repo.refs["HEAD"] = commit.id
-        return commit.id
-
     def __init__(self, path):
         # Creating the repository
-        if os.path.isdir(path):
+        if isdir(path):
             self.repo = Repo(path)
         else:
-            os.makedirs(path)
+            makedirs(path)
             self.repo = Repo.init(path)
-            self._commit(Tree())
 
     # Only this function will be used on upload
-    def change(self, files):
+    def stage(self, files, modified=False):
         """
-        used to change a file in the git storage can be called for the first
-        upload we don't care
+        add files to the staging area before commit.
 
         ``files``
-            a list of file to change
+            a list of file to stage
+        ``modified``
+            if True, will stage pending modifications (useful for staging
+            deletes)
         """
+        if modified:
+            files.update(set(get_unstaged_changes(self.repo.open_index(),
+                                                  'sources')))
+
         if len(files) != 0:
-            self.repo.stage(files)
-            self.repo.do_commit("this is so awesome that nobody will never see "
-                                "it", committer="same here <foo@foo.foo>")
+            self.repo.stage(list(files))
+
+    def commit(self):
+        ref = self.repo.do_commit(b'this is so awesome that nobody will never '
+                                  b'see it',
+                                  committer=b'same here <foo@foo.foo>')
+
+        return ref.decode()
 
     def buildTreeDiff(self, old_sha_tree=None, new_sha_tree=None):
         """
@@ -142,37 +169,3 @@ class GitBackendDulwich():
         for c in self.repo.get_parents(commit):
             result.append(self.repo.get_object(c).tree)
         return result
-
-    def getOlderFileContent(self, filename):
-        """
-        return the first filename's content that changed from the filename
-        ``filename``
-            the filename to work on
-        """
-        # Get a list of Tree (from the newest to the oldest)
-        # and skip the current one
-        last_tree_with_changes = None
-        history = self.getAllTrees()
-        history.pop(0)
-
-        # Find the first Tree that contains our file
-        for sha in history:
-            tree = self.repo.get_object(sha)
-            if filename in tree:
-                last_tree_with_changes = tree
-                break
-
-        # File has no previous history or does not exist in the repo
-        if not last_tree_with_changes:
-            raise NoOlderContent("{} has no previous version".format(filename))
-
-        # Get corresponding blob and return its content
-        (_, sha) = last_tree_with_changes[filename]
-        blob = self.repo.get_object(sha)
-        return blob.as_raw_string()
-
-    def getOlderCommits(self):
-        """
-        return a list of all commits
-        """
-        return self.repo.get_parents(self.repo.head())
