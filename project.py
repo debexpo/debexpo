1374918281dd7f628b4a10ba475a9929655050d8
Mattia Rizzolo <mattia@debian.org>
2020-07-20 18:02:21 +0200

    Use "3.0" instead of "3.0.0" as project version.  We are not going to use semver here
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- project.py
+++ project.py
@@ -3,6 +3,6 @@ Common debexpo variable to be used in any files that requires that informations
 """
 
 PROJECT = 'debexpo'
-VERSION = '3.0.0'
+VERSION = '3.0'
 AUTHOR = 'Debexpo maintainers (see AUTHORS)'
 COPYRIGHT = '2008-2020, {}'.format(AUTHOR)
