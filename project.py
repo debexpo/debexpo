cbb8e24bde465df9fcf614d66259ad9d4c7a554f
Mattia Rizzolo <mattia@debian.org>
2020-07-20 18:02:08 +0200

    Bump copyright years to 2020
    Signed-off-by: Mattia Rizzolo <mattia@debian.org>

--- project.py
+++ project.py
@@ -5,4 +5,4 @@ Common debexpo variable to be used in any files that requires that informations
 PROJECT = 'debexpo'
 VERSION = '3.0.0'
 AUTHOR = 'Debexpo maintainers (see AUTHORS)'
-COPYRIGHT = '2008-2019, {}'.format(AUTHOR)
+COPYRIGHT = '2008-2020, {}'.format(AUTHOR)
