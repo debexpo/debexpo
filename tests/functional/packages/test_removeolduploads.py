46e07aa70c1681b591d192944d70de7de954f0cd
Baptiste BEAUPLAT <lyknode@cilg.org>
2020-07-25 22:08:48 +0200

    Add regression tests for multiples version in NEW packages list

--- tests/functional/packages/test_removeolduploads.py
+++ tests/functional/packages/test_removeolduploads.py
@@ -132,14 +132,18 @@ class TestCronjobRemoveOldUploads(TestController):
 
         self.state = new_state
 
-    def remove_upload_accepted(self, uploaded, down=False, garbage=False):
+    def remove_upload_accepted(self, uploaded, down=False, garbage=False,
+                               handler=None):
         removed_packages = (
             ('tmux', '1.0.0', 'unstable'),
             ('tmux', '1.0.0', 'UNRELEASED'),
         )
 
+        if not handler:
+            handler = FTPMasterPackageInNewHTTPHandler
+
         self._expect_package_removal(removed_packages)
-        with TestingHTTPServer(FTPMasterPackageInNewHTTPHandler) as httpd:
+        with TestingHTTPServer(handler) as httpd:
             with self.settings(
                     FTP_MASTER_NEW_PACKAGES_URL='http://localhost:'
                                                 f'{httpd.port}'):
@@ -154,6 +158,15 @@ class TestCronjobRemoveOldUploads(TestController):
                     FTP_MASTER_NEW_PACKAGES_URL='http://localhost:'
                                                 f'{httpd.port}'):
                 remove_uploaded_packages(FakeNNTPClient([]))
+        self._assert_cronjob_success()
+
+    def test_package_in_new_double_version(self):
+        self._setup_packages()
+
+        self.remove_upload_accepted(
+            [],
+            handler=FTPMasterPackageInNewDoubleVersionHTTPHandler
+        )
         self._assert_cronjob_success()
 
     def test_remove_uploads_noop_no_packages(self):
@@ -215,6 +228,14 @@ class TestCronjobRemoveOldUploads(TestController):
         self.remove_upload_accepted([('htop', '0.9.0', 'unstable')])
         self._expect_package_removal(removed_packages)
         self._assert_cronjob_success()
+
+
+class FTPMasterPackageInNewDoubleVersionHTTPHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200, 'OK')
+        self.end_headers()
+        self.wfile.write(bytes(PACKAGE_IN_NEW.replace('1.0.0', '0.9.0 1.0.0'),
+                               'UTF-8'))
 
 
 class FTPMasterPackageInNewHTTPHandler(BaseHTTPRequestHandler):
