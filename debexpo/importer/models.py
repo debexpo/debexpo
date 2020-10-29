8e310b73fb19d8b1780436c130d395e38b837969
Baptiste Beauplat <lyknode@cilg.org>
2020-10-29 23:06:29 +0100

    Prevent uploads from equal or lower versions than already uploaded in Debian

--- debexpo/importer/models.py
+++ debexpo/importer/models.py
@@ -426,6 +426,7 @@ class Importer():
             changes.validate()
             if not self.skip_gpg:
                 changes.authenticate()
+            changes.assert_newer()
             changes.files.validate()
             changes.get_bugs()
         except (ExceptionChanges, ExceptionCheckSumedFile, ExceptionGnuPG) as e:
