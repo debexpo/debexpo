c7538a571b26a9f0c657b9ff6b7fafab55ca2713
Baptiste Beauplat <lyknode@debian.org>
2023-07-09 16:14:11 +0200

    Add new command sync_distribution (Closes: #151)

--- debexpo/settings/common.py
+++ debexpo/settings/common.py
@@ -265,3 +265,24 @@ SUBPROCESS_TIMEOUT_LINTIAN = 30 * 60
 
 # Default settings for models
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Distro-info vendor info
+DISTRO_INFO_VENDOR = 'debian'
+DISTRIBUTION_SUFFIX = (
+    '-backports',
+    '-backports-sloppy',
+    '-security',
+    '-updates',
+    '-proposed-updates',
+)
+STATIC_SUITES = (
+    'sid',
+    'experimental',
+)
+SUITE_ALIASES = {
+    'oldstable': None,
+    'stable': None,
+    'testing': None,
+    'unstable': None,
+    'UNRELEASED': 'sid',
+}
